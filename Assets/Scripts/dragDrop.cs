79f7273fa6e4a4c34786f99cfadd85bc4bbab5db
cdilday <cdilday@ucsc.edu>
2014-05-03 11:53:24 -0700

    Imported game commit
    one last go here

---
+++ Assets/Scripts/dragDrop.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+
+
+class dragDrop : MonoBehaviour
+	
+{
+	//
+	private Color mouseOverColor = Color.blue;
+	
+	private Color originalColor ;
+	
+	private bool dragging = false;
+	
+	private float distance;
+	
+	
+	
+	
+	
+	void OnMouseEnter()
+
+	{
+		originalColor = gameObject.transform.parent.renderer.material.GetColor ("_Color");
+		gameObject.transform.parent.renderer.material.color = mouseOverColor;
+		
+	}
+	
+	
+	
+	void OnMouseExit()
+		
+	{
+		
+		gameObject.transform.parent.renderer.material.color = originalColor;
+		
+	}
+	
+	
+
+	void OnMouseDown()
+		
+	{
+		
+		distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+		
+		dragging = true;
+		
+	}
+	
+	
+	
+	void OnMouseUp()
+		
+	{
+		
+		dragging = false;
+		
+	}
+	
+	
+	
+	void Update()
+		
+	{
+		
+		if (dragging)
+			
+		{
+			
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			
+			Vector3 rayPoint = ray.GetPoint(distance);
+			
+			transform.parent.position = rayPoint;
+			transform.position = rayPoint;
+
+		}
+		
+	}
+	
+}
